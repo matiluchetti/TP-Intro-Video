ffe760b3436148721caeddd6a948a6c17a1b35f0
Matias Luchetti <mluchetti@itba.edu.ar>
2025-05-18 18:47:28 -0300

    Added Empty Mag Sound to Shotgun

--- Assets/Scripts/Weapons/Shotgun.cs
+++ Assets/Scripts/Weapons/Shotgun.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Shotgun : Gun
 {
     [SerializeField] private int _shotCount = 5;
+    [SerializeField] private AudioClip _emptyClip;
 
     public override void Attack()
     {
+        if (_shotCount <= 0 || _currentBulletCount <= 0)
+        {
+            Debug.Log("No hay balas disponibles.");
+            if (_emptyClip != null && _audioSource != null)
+            {
+                _audioSource.PlayOneShot(_emptyClip);
+            }
+            return;
+        }
         for (int i = 0; i < _shotCount; i++)
         {
             if (_currentBulletCount <= 0)
