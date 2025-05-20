4610c9bc44d11f54bb27fa6bd36f807a7885aef5
Mariano Agopian <maagopian@itba.edu.ar>
2025-05-20 12:51:56 -0300

    addeed loading scene

---
+++ Assets/Scripts/Controllers/LoadingController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingController : MonoBehaviour {
+  public GameObject LoadingScreen;
+  public Slider Slider;
+
+  public void LoadScene(int sceneId) {
+    StartCoroutine(LoadSceneAsync(sceneId));
+  }
+
+  IEnumerator LoadSceneAsync(int sceneId) {
+    AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+
+    LoadingScreen.SetActive(true);
+
+    while (!operation.isDone) {
+      float progress = Mathf.Clamp01(operation.progress / .9f);
+      Slider.value = progress;
+      yield return null;
+    }
+  }
+}
--- Assets/Scripts/Controllers/StartMenuController.cs
+++ Assets/Scripts/Controllers/StartMenuController.cs
@@ -2,10 +2,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartMenuController : MonoBehaviour {
-    public void onStartClick() {
-        SceneManager.LoadScene("GameScene");
-    }
-
     public void onExitClick() {
         #if UNITY_EDITOR
                 UnityEditor.EditorApplication.isPlaying = false;
